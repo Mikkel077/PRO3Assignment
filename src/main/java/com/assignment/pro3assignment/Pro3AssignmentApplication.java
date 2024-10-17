bb6a3aa8d03a4bb48050b182485b39c20ac3cfdb
mikkel077 <mikkelhansen077@gmail.com>
2024-10-17 11:11:39 +0200

    init

--- src/main/java/com/assignment/pro3assignment/Pro3AssignmentApplication.java
+++ src/main/java/com/assignment/pro3assignment/Pro3AssignmentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Pro3AssignmentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Pro3AssignmentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Pro3AssignmentApplication.class, args);
+    }
 
 }
